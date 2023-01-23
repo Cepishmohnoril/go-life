19373b16998ce72a8be7b14f9d3f1d6c44eb859d
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2023-01-23 17:53:53 +0200

    initiall commit, gtk installed

---
+++ life.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gotk3/gotk3/gtk"
+)
+
+func main() {
+
+	gtk.Init(nil)
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
+	}
+	win.SetTitle("Simple Example")
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	l, err := gtk.LabelNew("Hello, gotk3!")
+	if err != nil {
+		log.Fatal("Unable to create label:", err)
+	}
+
+	win.Add(l)
+	win.SetDefaultSize(800, 600)
+	win.ShowAll()
+	gtk.Main()
+
+}
