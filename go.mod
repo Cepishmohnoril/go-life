35832768c35281ea77385c3115ac396194b3a92f
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2023-01-25 18:34:14 +0200

    fyne instead of gtk

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/Cepishmohnoril/go-life
 
 go 1.19
 
-require github.com/gotk3/gotk3 v0.6.1 // indirect
+require (
+	fyne.io/fyne/v2 v2.3.0 // indirect
+)
--- life.go
+++ life.go
@@ -1,31 +1,34 @@
 package main
 
 import (
-	"log"
+	"image/color"
+	"time"
 
-	"github.com/gotk3/gotk3/gtk"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 )
 
 func main() {
+	createWindow()
+}
 
-	gtk.Init(nil)
-	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-	if err != nil {
-		log.Fatal("Unable to create window:", err)
-	}
-	win.SetTitle("Simple Example")
-	win.Connect("destroy", func() {
-		gtk.MainQuit()
-	})
+func createWindow() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Canvas")
+	myCanvas := myWindow.Canvas()
 
-	l, err := gtk.LabelNew("Hello, gotk3!")
-	if err != nil {
-		log.Fatal("Unable to create label:", err)
-	}
+	blue := color.NRGBA{R: 0, G: 0, B: 180, A: 255}
+	rect := canvas.NewRectangle(blue)
+	myCanvas.SetContent(rect)
 
-	win.Add(l)
-	win.SetDefaultSize(800, 600)
-	win.ShowAll()
-	gtk.Main()
+	go func() {
+		time.Sleep(time.Second)
+		green := color.NRGBA{R: 0, G: 180, B: 0, A: 255}
+		rect.FillColor = green
+		rect.Refresh()
+	}()
 
+	myWindow.Resize(fyne.NewSize(100, 100))
+	myWindow.ShowAndRun()
 }
